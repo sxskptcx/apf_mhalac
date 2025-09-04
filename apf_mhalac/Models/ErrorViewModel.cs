fa0070a593cb23e37c412cf60eb9b3988cb4a9fc
sxcr <heyiamrobot@gmail.com>
2025-09-04 11:49:00 +0200

    Add project files.

---
+++ apf_mhalac/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace apf_mhalac.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
