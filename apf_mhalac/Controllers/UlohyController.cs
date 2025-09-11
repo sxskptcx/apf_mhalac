8aca81eab1d8fd64d25d2fcdbcbcd3a742b879c7
sxcr <heyiamrobot@gmail.com>
2025-09-11 13:23:17 +0200

    kokot vyjebany

--- apf_mhalac/Controllers/UlohyController.cs
+++ apf_mhalac/Controllers/UlohyController.cs
@@ -1,10 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using apf_mhalac.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace apf_mhalac.Controllers
 {
     public class UlohyController : Controller
     {
         public IActionResult Uloha1()
+        {
+            var userList = new List<UserInfo>()
+            {
+                new UserInfo 
+                { 
+                    Name = "Test1", 
+                    Surname = "Novotny",
+                    Email = "1@gmail.com" 
+                },
+                new UserInfo
+                {
+                    Name = "Test2",
+                    Surname = "Grezdo",
+                    Email = "2@gmail.com"
+                },
+                new UserInfo
+                {
+                    Name = "Test3",
+                    Surname = "Kovacs",
+                    Email = "3@gmail.com"
+                }
+            };
+            return View(userList);
+        }
+
+        public IActionResult Uloha2()
+        {
+            return View();
+        }
+
+        public IActionResult Uloha3()
         {
             return View();
         }
---
+++ apf_mhalac/Models/UserInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace apf_mhalac.Models
+{
+    public class UserInfo
+    {
+        public string Name { get; set; } = "";
+        public string Surname { get; set; } = "";
+
+        public string Email { get; set; } = string.Empty;
+
+    }
+}
