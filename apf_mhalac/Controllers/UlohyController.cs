f2f74167e796d1cd26804c7fb280179b47d36c20
sxcr <heyiamrobot@gmail.com>
2025-09-11 10:57:05 +0200

    Uloha1

---
+++ apf_mhalac/Controllers/UlohyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace apf_mhalac.Controllers
+{
+    public class UlohyController : Controller
+    {
+        public IActionResult Uloha1()
+        {
+            return View();
+        }
+    }
+}
